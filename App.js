4cc36566d380400c560560463cafa7f540ecbc1d
Pedro Soto <SotoPedro>
2021-08-19 20:00:03 -0500

    App location to found Bi Bici stations

--- App.js
+++ App.js
@@ -1,12 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { BICI_PLACES } from './Data/GasStations';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  //Locations
+  const [startLoc, setStartLoc] = React.useState({  
+    latitude: 20.624620886817652,
+    longitude: -103.3565319312955,
+    latitudeDelta: 0.0922, 
+    longitudeDelta: 0.0421
+  });
+  const [CurrLoc, setCurrLoc] = React.useState({  
+    latitude: 20.624620886817652,
+    longitude: -103.3565319312955
+  }); 
+  //Stations 
+  const [staInfMB, setInfMB] = React.useState([]);
+  //const [stationStatus, setStationStatus] = React.useState({}); 
+  //const [statStaMB, setStaMB] = React.useState([]);
+
+  const fetchStatus = async () => {
+      let response = await fetch('https://guad.publicbikesystem.net/ube/gbfs/v1/');
+      let data = await response.json();       
+      
+      let urlInfo = await fetch(data.data.en.feeds[0].url)
+      let stationsMB =await urlInfo.json();              
+      setInfMB(stationsMB.data.stations);
+  }
+ 
+  React.useEffect(() => {
+         fetchStatus();            
+  }, []);
+ 
+   return (
+    <View style={styles.container}>   
+      <MapView style={styles.map} 
+        initialRegion={startLoc}
+        provider="google"
+      >       
+        {staInfMB.map((stations) => {
+          return (
+            <View key={stations.station_id} >
+              <Marker 
+                coordinate={{
+                  latitude: stations.lat,
+                  longitude: stations.lon
+                }}
+                pinColor="blue"
+              >
+                <Callout>
+                  <Text>
+                    { 
+                       stations.name
+                    }
+                  </Text>
+                </Callout>
+              </Marker>
+            </View>
+          )       
+        })}
+        <Marker 
+          coordinate={CurrLoc}
+          pinColor="black"
+          draggable={true}        
+          onDragEnd={e => {
+            setCurrLoc({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude
+            })
+          }}
+        >
+          <Callout>
+            <Text>Hi, i'm here</Text>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -17,5 +86,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
